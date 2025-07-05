b2ffe5ae28dc7f8e0ba3b7c244c3ae16ec0fb21a
Lennart Pieperjohanns <95208734+lennartpj@users.noreply.github.com>
2025-07-05 06:36:40 -0400

    Alacritty setup

---
+++ alacritty/setup.sh
@@ -0,0 +1,40 @@
+installAlacritty () {
+	sudo dnf install alacritty
+}
+
+
+setupAlacrittyConfig() {
+    printf "%b\n" "${YELLOW}Copying alacritty config files...${RC}"
+    if [ -d "${HOME}/.config/alacritty" ] && [ ! -d "${HOME}/.config/alacritty-bak" ]; then
+        cp -r "${HOME}/.config/alacritty" "${HOME}/.config/alacritty-bak"
+    fi
+    mkdir -p "${HOME}/.config/alacritty/"
+    cp -r ./* "${HOME}/.config/alacritty/"
+    printf "%b\n" "${GREEN}Alacritty configuration files copied.${RC}"
+}
+
+
+installFont() {
+    # Check to see if the MesloLGS Nerd Font is installed (Change this to whatever font you would like)
+    FONT_NAME="MesloLGS Nerd Font Mono"
+    if fc-list :family | grep -iq "$FONT_NAME"; then
+        printf "%b\n" "${GREEN}Font '$FONT_NAME' is installed.${RC}"
+    else
+        printf "%b\n" "${YELLOW}Installing font '$FONT_NAME'${RC}"
+        # Change this URL to correspond with the correct font
+        FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
+        FONT_DIR="$HOME/.local/share/fonts"
+        TEMP_DIR=$(mktemp -d)
+        curl -sSLo "$TEMP_DIR"/"${FONT_NAME}".zip "$FONT_URL"
+        unzip "$TEMP_DIR"/"${FONT_NAME}".zip -d "$TEMP_DIR"
+        mkdir -p "$FONT_DIR"/"$FONT_NAME"
+        mv "${TEMP_DIR}"/*.ttf "$FONT_DIR"/"$FONT_NAME"
+        fc-cache -fv
+        rm -rf "${TEMP_DIR}"
+        printf "%b\n" "${GREEN}'$FONT_NAME' installed successfully.${RC}"
+    fi
+}
+
+installAlacritty
+setupAlacrittyConfig
+installFont
